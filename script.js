85ed509c720fa84901c30470f7bd06550f88a24c
ugurduyar <ismailugurduyar@gmail.com>
2020-06-06 21:27:53 +0300

    init-images added

---
+++ script.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Card options
+  const cardArray = [
+    {
+      name: "dog",
+      img: "images/dog.png",
+    },
+    {
+      name: "dog",
+      img: "images/dog.png",
+    },
+    {
+      name: "dog2",
+      img: "images/dog2.png",
+    },
+    {
+      name: "dog2",
+      img: "images/dog2.png",
+    },
+    {
+      name: "bee",
+      img: "images/bee.png",
+    },
+    {
+      name: "bee",
+      img: "images/bee.png",
+    },
+    {
+      name: "cat",
+      img: "images/cat.png",
+    },
+    {
+      name: "cat",
+      img: "images/cat.png",
+    },
+    {
+      name: "cow",
+      img: "images/cow.png",
+    },
+    {
+      name: "cow",
+      img: "images/cow.png",
+    },
+    {
+      name: "deer",
+      img: "images/deer.png",
+    },
+    {
+      name: "deer",
+      img: "images/deer.png",
+    },
+    {
+      name: "black",
+      img: "images/black.png",
+    },
+    {
+      name: "black",
+      img: "images/black.png",
+    },
+    {
+      name: "blank",
+      img: "images/blank.png",
+    },
+    {
+      name: "blank",
+      img: "images/blank.png",
+    },
+  ];
+});
